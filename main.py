046b2ce12172877d1718ff68df4db9dc3bc24240
Sreeteja1 <87115122+Sreeteja1@users.noreply.github.com>
2025-05-29 12:42:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,14 +31,15 @@ else:
             "Report generation taking longer than usual",    # Medium
             "Want to update my contact info",                # Low
             "Getting intermittent errors in dashboard" ,     # Medium
-            "Working fine"                                   #Low
+            "Working fine",                                  #Low
+           "API is not working"                              #High
     ],
     'Urgency': [
         
             "High", "Low", "High", "Medium", "Low",
             "Medium", "High", "Low", "Medium", "Low",
             "High", "Medium", "Medium", "Low", "Medium",
-            "Low"
+            "Low","High"
     ]
     })
 
